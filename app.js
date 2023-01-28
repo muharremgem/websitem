676e3bffdb41b65d15415241896b02e7e222f075
muharrem gem <muharremgem28@gmail.com>
2023-01-28 03:03:33 +0300

    email

--- app.js
+++ app.js
@@ -163,7 +163,7 @@ function sendMail() {
       document.getElementById("email").value = "";
       document.getElementById("message").value = "";
       console.log(res);
-      // alert("Your message sent successfully!!");
+      alert("Your message sent successfully!!");
     })
     .catch((err) => console.log(err));
 
